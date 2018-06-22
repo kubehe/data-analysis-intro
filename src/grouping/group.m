4c628407bc221f9ac73bf4db82645b63eaf05969
Inf <50wilk@gmail.com>
2018-06-22 22:24:43 +0200

    grupowanko

---
+++ src/grouping/group.m
@@ -0,0 +1,24 @@
+close all
+clear
+
+load data/dataSet.mat
+
+% zamiana na tablice
+data_array = table2array(t);
+% usuniêcie ostatniej kolumny z klasami
+no_cls_array = data_array(:,1:end-1);
+
+% odleg³oœci miêdzy obiektami
+dist = pdist(no_cls_array);
+l1 = linkage(no_cls_array,'single');
+l2 = linkage(no_cls_array,'complete');
+l3 = linkage(no_cls_array,'average');
+
+% wykresy dendrogramów
+% najwyraŸniejszy podzia³ dla single linkage
+figure; dendrogram(l1, 'colorthreshold', 'default');
+title("Dendrogram dla single");
+figure; dendrogram(l2, 'colorthreshold', 'default');
+title("Dendrogram dla complete");
+figure; dendrogram(l3, 'colorthreshold', 'default');
+title("Dendrogram dla average");
