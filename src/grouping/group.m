25a6f7cd52377c89e79c083e989ff32c2a26c0db
Inf <50wilk@gmail.com>
2018-06-25 18:44:11 +0200

    zmiana svg na png dla macierzy wykresow (za duzo elementow)

--- src/grouping/group.m
+++ src/grouping/group.m
@@ -18,7 +18,7 @@ figure;
 gplotmatrix(std_nocls, [], data_array(:,end), [1 0 0; 0 1 0; 0 0 1; 1 0.85 0.25; 0.75 0.35 0.45], '.', 4);   
 title("Wykresy punktowe atrybutów z podzia³em wg atrybutu decyzyjnego");
 set(gcf, 'PaperUnits', 'centimeters', 'PaperPosition', [0 0 25 15]);
-saveas(gcf, '../../output/plotmat-classes', 'svg');
+saveas(gcf, '../../output/plotmat-classes', 'png');
 
 % odleglosci miedzy obiektami dla atr. 4, 6 i 9
 diststd = pdist(std_nocls(:,[4 6 9]));
@@ -46,8 +46,8 @@ saveas(gcf, '../../output/dgram-average', 'svg');
 k2 = kmeans(std_nocls(:,[4 6 9]), 5, 'Replicates', 150);
 figure; gplotmatrix(std_nocls, [], k2, [1 0 0; 0 1 0; 0 0 1; 1 0.85 0.25; 0.75 0.35 0.45], '.', 4); 
 title("Wykresy punktowe atrybutów 4, 6 i 9 z grupowaniem (kmeans)");
-set(gcf, 'PaperUnits', 'centimeters', 'PaperPosition', [0 0 35 20]);
-saveas(gcf, '../../output/plotmat-group469-kmeans', 'svg');
+set(gcf, 'PaperUnits', 'centimeters', 'PaperPosition', [0 0 25 15]);
+saveas(gcf, '../../output/plotmat-group469-kmeans', 'png');
 
 % grupowanie wg odleglosci dla atr. [4 6 9]
 % dla complete i average wychodzi niemal identycznie jak wg podanych klas
@@ -57,10 +57,10 @@ c3 = clusterdata(std_nocls(:,[4 6 9]), 'linkage', 'single', 'maxclust', 5);
 figure; gplotmatrix(std_nocls, [], c1, [1 0 0; 0 1 0; 0 0 1; 1 0.85 0.25; 0.75 0.35 0.45], '.', 4); 
 title("Wykresy punktowe atrybutów 4, 6 i 9 z grupowaniem (average linkage)");
 set(gcf, 'PaperUnits', 'centimeters', 'PaperPosition', [0 0 25 15]);
-saveas(gcf, '../../output/plotmat-group469-avglink', 'svg');
+saveas(gcf, '../../output/plotmat-group469-avglink', 'png');
 
 % porownanie wynikow grupowania roznymi metodami do atrybutu decyzyjnego
-%cross_link_av = crosstab(c1, data_array(:,end));
-%cross_link_com = crosstab(c2, data_array(:,end));
-%cross_link_single = crosstab(c3, data_array(:,end));
-%cross_kmeans = crosstab(k2, data_array(:,end));
+cross_link_av = crosstab(c1, data_array(:,end));
+cross_link_com = crosstab(c2, data_array(:,end));
+cross_link_single = crosstab(c3, data_array(:,end));
+cross_kmeans = crosstab(k2, data_array(:,end));
