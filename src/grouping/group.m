cae0d6a04f067e4621b409f01274cfbc811f9854
Inf <50wilk@gmail.com>
2018-06-25 18:07:16 +0200

    zamiana pdf na svg

--- src/grouping/group.m
+++ src/grouping/group.m
@@ -17,7 +17,8 @@ std_nocls = zscore(no_cls_array);
 figure;
 gplotmatrix(std_nocls, [], data_array(:,end), [1 0 0; 0 1 0; 0 0 1; 1 0.85 0.25; 0.75 0.35 0.45], '.', 4);   
 title("Wykresy punktowe atrybutów z podzia³em wg atrybutu decyzyjnego");
-saveas(gcf, '../../output/plotmat-classes', 'pdf');
+set(gcf, 'PaperUnits', 'centimeters', 'PaperPosition', [0 0 25 15]);
+saveas(gcf, '../../output/plotmat-classes', 'svg');
 
 % odleglosci miedzy obiektami dla atr. 4, 6 i 9
 diststd = pdist(std_nocls(:,[4 6 9]));
@@ -29,20 +30,24 @@ ls3 = linkage(diststd,'average');
 % najwyrazniejszy podzia³ dla complete i average linkage
 figure; dendrogram(ls1, 20, 'colorthreshold', 'default');
 title("Dendrogram grupowania hierarchicznego (single linkage)");
-saveas(gcf, '../../output/dgram-single', 'pdf');
+set(gcf, 'PaperUnits', 'centimeters', 'PaperPosition', [0 0 25 15]);
+saveas(gcf, '../../output/dgram-single', 'svg');
 figure; dendrogram(ls2, 20, 'colorthreshold', 'default');
 title("Dendrogram grupowania hierarchicznego (complete linkage)");
-saveas(gcf, '../../output/dgram-complete', 'pdf');
+set(gcf, 'PaperUnits', 'centimeters', 'PaperPosition', [0 0 25 15]);
+saveas(gcf, '../../output/dgram-complete', 'svg');
 figure; dendrogram(ls3, 20, 'colorthreshold', 'default');
 title("Dendrogram grupowania hierarchicznego (average linkage)");
-saveas(gcf, '../../output/dgram-average', 'pdf');
+set(gcf, 'PaperUnits', 'centimeters', 'PaperPosition', [0 0 25 15]);
+saveas(gcf, '../../output/dgram-average', 'svg');
 
 % grupowanie dla atrybutów 4, 6 i 9 
 % k-srednie 
 k2 = kmeans(std_nocls(:,[4 6 9]), 5, 'Replicates', 150);
 figure; gplotmatrix(std_nocls, [], k2, [1 0 0; 0 1 0; 0 0 1; 1 0.85 0.25; 0.75 0.35 0.45], '.', 4); 
 title("Wykresy punktowe atrybutów 4, 6 i 9 z grupowaniem (kmeans)");
-saveas(gcf, '../../output/plotmat-group469-kmeans', 'pdf');
+set(gcf, 'PaperUnits', 'centimeters', 'PaperPosition', [0 0 35 20]);
+saveas(gcf, '../../output/plotmat-group469-kmeans', 'svg');
 
 % grupowanie wg odleglosci dla atr. [4 6 9]
 % dla complete i average wychodzi niemal identycznie jak wg podanych klas
@@ -51,10 +56,11 @@ c2 = clusterdata(std_nocls(:,[4 6 9]), 'linkage', 'complete', 'maxclust', 5);
 c3 = clusterdata(std_nocls(:,[4 6 9]), 'linkage', 'single', 'maxclust', 5);
 figure; gplotmatrix(std_nocls, [], c1, [1 0 0; 0 1 0; 0 0 1; 1 0.85 0.25; 0.75 0.35 0.45], '.', 4); 
 title("Wykresy punktowe atrybutów 4, 6 i 9 z grupowaniem (average linkage)");
-saveas(gcf, '../../output/plotmat-group469-avglink', 'pdf');
+set(gcf, 'PaperUnits', 'centimeters', 'PaperPosition', [0 0 25 15]);
+saveas(gcf, '../../output/plotmat-group469-avglink', 'svg');
 
 % porownanie wynikow grupowania roznymi metodami do atrybutu decyzyjnego
-cross_link_av = crosstab(c1, data_array(:,end));
-cross_link_com = crosstab(c2, data_array(:,end));
-cross_link_single = crosstab(c3, data_array(:,end));
-cross_kmeans = crosstab(k2, data_array(:,end));
+%cross_link_av = crosstab(c1, data_array(:,end));
+%cross_link_com = crosstab(c2, data_array(:,end));
+%cross_link_single = crosstab(c3, data_array(:,end));
+%cross_kmeans = crosstab(k2, data_array(:,end));
